815157670fabe832a8629b9ef73bf65535b8e5ea
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-01-19 23:19:37 -0500

    whatevers

--- lib/edmunds_vin.rb
+++ lib/edmunds_vin.rb
@@ -2,6 +2,24 @@ require "edmunds_vin/version"
 require 'net/http'
 require 'json'
 
+VinData = Struct.new(:make, :model, :year, :engine, :transmission, :fuel)
+VinParser = -> raw {
+  VinData.new(
+    raw['make']['name'],
+    raw['model']['name'],
+    raw['years'][0]['year'],
+    {
+      'type' => raw['engine']['name'],
+      'code' => raw['engine']['manufacturerEngineCode']
+    },
+    {
+      'type' => raw['transmission']['transmissionType'],
+      'code' => raw['transmission']['name']
+    },
+    raw['engine']['type'],
+  )
+}
+
 module EdmundsVin
 
   class Decoder
@@ -16,7 +34,10 @@ module EdmundsVin
       raise ArgumentError unless vin.length == 17
       query_url = URI("https://api.edmunds.com/api/vehicle/v2/vins/#{vin}?fmt=#{@format}&api_key=#{@key}")
       response = Net::HTTP.get(query_url)
-      response
+      decoded = JSON.parse(response)
+      formatted = VinParser.call(decoded)
+
+      formatted
     end
 
   end
